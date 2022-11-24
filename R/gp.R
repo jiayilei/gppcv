8159f0b9e8bfe609e3276b83245e3ea61b438da2
Jiayi Lei <leoi06012015@gmail.com>
2022-11-23 23:22:57 -0600

    skeleton for the gaussian process regression function.

---
+++ R/gp.R
@@ -0,0 +1,25 @@
+source(utilities.R)
+#' Title
+#'
+#' @param X inputs
+#' @param y targets
+#' @param k covariance function
+#' @param sigma2 noise level
+#' @param Xt test input
+#'
+#' @return
+#'        fs mean function
+#'        Vfs variance
+#'        logp log marginal likelihood
+#' @export
+#'
+#' @examples
+gpr <- function(X, y, k, sigma2, Xt){
+  # cholesky factorization
+
+  # find predictive mean eq.
+
+  # find predictive variance eq.
+
+  # calculate log marginal likelihood
+}
