6d09597e8a3530b087c7cc76397fa3d94ac59adc
Jiayi Lei <leoi06012015@gmail.com>
2022-11-23 23:23:10 -0600

    skeleton of the utilitie function.

---
+++ R/utilities.R
@@ -0,0 +1,28 @@
+
+#' Title
+#'
+#' @param theta GP parameter
+#' @param X training data
+#' @param y response data
+#'
+#' @return theta
+#' @export
+#'
+#' @examples TODO
+get_MAP <- function(theta, X, y){
+
+}
+
+#' Title
+#'
+#' @param theta GP parameter
+#' @param X training data
+#' @param y response data
+#'
+#' @return theta
+#' @export
+#'
+#' @examples TODO
+get_MMSE <- function(theta, X, y){
+
+}
