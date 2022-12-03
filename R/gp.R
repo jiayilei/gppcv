9ea40110b3730968acc76bc0616493a7aabab968
Jiayi Lei <leoi06012015@gmail.com>
2022-12-03 12:44:20 -0600

    implemented prediction and log marginal likelihood for Gaussian Process regression based on Carl Edward's algorithm.

--- R/gp.R
+++ R/gp.R
@@ -1,11 +1,11 @@
-source(utilities.R)
+# source("utilities.R")
 #' Title
 #'
 #' @param X inputs
 #' @param y targets
 #' @param k covariance function
 #' @param sigma2 noise level
-#' @param Xt test input
+#' @param xt test input, 1 dimensional test input data
 #'
 #' @return
 #'        fs mean function
@@ -14,12 +14,48 @@ source(utilities.R)
 #' @export
 #'
 #' @examples
-gpr <- function(X, y, k, sigma2, Xt){
+gpr <- function(X, y, k, sigma2, xt){
+  # compatibility check
+  n = dim(X)[1]
+  p = dim(X)[2]
+
+  if (length(y) != n){
+    stop("dimension of X and Y does not match")
+  }
+  if (dim(xt)[2] != p){
+    stop("dimension of X and Xt does not match")
+  }
+  if (sigma2 < 0){
+    stop("Sigma2 needs to be positive")
+  }
+
+  # covariance matrix: covariance evaluated at all pairs of training point
+  K = matrix(rep(0,n*n), n,n)
+  # covariance vector: covariance between test point and the n training points
+  ks = matrix(rep(0,n),n)
+  for (i in (1:n)){
+    for (j in (i:n)){
+      K[i,j] = k(X[i,], X[j,])
+    }
+    ks[i] = k(X[i,], xt)
+  }
+  tK = t(K)
+  diag(tK) <- 0
+  K = tK + K
+
   # cholesky factorization
+  L = chol(K + sigma2 * diag(n))
 
-  # find predictive mean eq.
+  # find predictive mean
+  alpha = solve(t(L)) %*% (solve(L) %*% y)
+  fs = t(ks) %*% alpha
 
-  # find predictive variance eq.
+  # find predictive variance
+  v = solve(v) %*% ks
+  Vfs = k(xt, xt) - crossprod(v)
 
   # calculate log marginal likelihood
+  logp = -1/2 * crossprod(y, alpha) - sum(log(diag(L))) - n/2 * log(2*pi)
+
+  return (fs, Vfs, logp)
 }
